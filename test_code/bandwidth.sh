6f8dc790061666c8399c54b4e7bfbcfee778be06
TsukasaYuki <admin@con.ee>
2018-07-16 19:01:03 +0800

    Update bandwidth.sh

--- test_code/bandwidth.sh
+++ test_code/bandwidth.sh
@@ -4,7 +4,7 @@ bandwidth()
 	apt install speedtest-cli
 	#wget --no-check-certificate https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py 1>/dev/null 2>&1
         #bd=`python speedtest.py --share --server 7456`
-        bd=`speedtest-cli --share --server 7456`
+        bd=`speedtest-cli --share --server 18445`
         download=`echo "$bd" | awk -F ':' '/Download/{print $2}'`
         upload=`echo "$bd" | awk -F ':' '/Upload/{print $2}'`
         hostby=`echo "$bd" | grep 'Hosted'`
@@ -18,7 +18,7 @@ bandwidth()
 
 
 	#寫入日誌文件
-	echo "===開始測試帶寬===">>${dir}/$logfilename
+	echo "===開始測試頻寬===">>${dir}/$logfilename
 	echo "$bd">>${dir}/$logfilename
-	echo -e "===帶寬測試結束==\n\n">>${dir}/$logfilename
+	echo -e "===頻寬測試結束==\n\n">>${dir}/$logfilename
 }
