e653c974d9745ef3b98087487edd10b654405a31
TsukasaYuki <admin@con.ee>
2018-07-23 03:03:22 +0800

    Update bandwidth.sh

--- test_code/bandwidth.sh
+++ test_code/bandwidth.sh
@@ -4,7 +4,8 @@ bandwidth()
 	apt install speedtest-cli
         #bd=`speedtest-cli --share --server 18445`
 	#bd=`speedtest-cli --share --server 4505`
-        bd=`speedtest-cli --share --server 10392`
+        #bd=`speedtest-cli --share --server 10392`
+        bd=`speedtest-cli --share`
         download=`echo "$bd" | awk -F ':' '/Download/{print $2}'`
         upload=`echo "$bd" | awk -F ':' '/Upload/{print $2}'`
         hostby=`echo "$bd" | grep 'Hosted'`
